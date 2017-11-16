394758e843df3875bb00cf5dad5c0c97a0ad8727
P-Nasty <31251496+P-Nasty@users.noreply.github.com>
2017-11-16 09:13:24 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -2,6 +2,9 @@ from flask import Flask, url_for, render_template, request, Markup, flash
 import os, json, random
 app = Flask(__name__)
 
+with open('skyscrapers.json') as skyscrapers_data:
+       buildings = json.load(skyscrapers_data)
+        
 @app.route("/")
 def render_main():
     return render_template('info.html')
