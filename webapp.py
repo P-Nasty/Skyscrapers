74204b7330fe9d381c938c5281216166b4dc0c25
Preston <prestongomersal@gmail.com>
2017-11-17 10:00:24 -0800

    jhdskjhfd

--- webapp.py
+++ webapp.py
@@ -5,9 +5,17 @@ app = Flask(__name__)
 with open('skyscrapers.json') as skyscrapers_data:
        buildings = json.load(skyscrapers_data)
         
-@app.route("/")
+@app.route("/home")
 def render_main():
     return render_template('info.html')
 
+@app.route("/data")
+def render_second():
+    return render_template('DataBySkyscraper.html')
+
+@app.route("/amount")
+def render_third():
+    return render_template('AmountOfSkyscrapers.html')
+    
 if __name__=="__main__":
-    app.run(debug=False, port=54321)
+    app.run(debug=True, port=54321)
